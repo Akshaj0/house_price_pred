ee0583042269503ad1bf24cb792cbc4713bb8701
Akshaj Alva <akshajalva0@gmail.com>
2024-09-16 10:35:12 +0530

    ML FLow

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## End to end house price prediction file
+
+MLFLOW_TRACKING_URI=https://dagshub.com/Akshaj0/house_price_prediction.mlflow
+MLFLOW_TRACKING_USERNAME=Akshaj0
+MLFLOW_TRACKING_PASSWORD=a6d547b7f6a925502135ea8795c4d3723443a9ed
--- application.py
+++ application.py
@@ -5,7 +5,8 @@ from src.house_price_prediction.components.data_ingestion import DataIngestion
 from src.house_price_prediction.components.data_ingestion import DataIngestionConfig
 from src.house_price_prediction.components.data_transformation import DataTransformation
 from src.house_price_prediction.components.data_transformation import DataTransformationConfig
-
+from src.house_price_prediction.components.model_trainer import ModelTrainer
+from src.house_price_prediction.components.model_trainer import ModelTrainerConfig
 
 if __name__ == "__main__":
     logging.error("We are testing logging")
@@ -15,7 +16,11 @@ if __name__ == "__main__":
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_array, test_array, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_array, test_array))
+
 
     except Exception as e:
         raise CustomException(e,sys)
